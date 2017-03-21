ec981456fbb40d3123fadc9479eb5bbec30ec488
peterosz <itharius@gmail.com>
2017-03-21 12:09:47 +0100

    pep8

--- TicTacToe_master.py
+++ TicTacToe_master.py
@@ -9,11 +9,11 @@ board = [None] + ['-' for x in range(9)]
 
 # Print the board
 def print_board(board):
-    print(' ',board[7],'|',board[8],'|',board[9])
+    print(' ', board[7], '|', board[8], '|', board[9])
     print(" ---+---+---")
-    print(' ',board[4],'|',board[5],'|',board[6])
+    print(' ', board[4], '|', board[5], '|', board[6])
     print(" ---+---+---")
-    print(' ',board[1],'|',board[2],'|',board[3])
+    print(' ', board[1], '|', board[2], '|', board[3])
 
 
 # Define the player X moves
@@ -22,7 +22,7 @@ def player1_move():
     while error == 1:
         try:
             player1_move = int(input(" Please take your move, select an empty space for X : "))
-            if player1_move==board[0] :
+            if player1_move == board[0]:
                 print("\nSorry, that\'s not a valid move. Try again! ")
                 time.sleep(1)
                 error = 1
@@ -41,15 +41,15 @@ def player1_move():
             time.sleep(1)
         os.system('clear')
         print_board(board)
-    
-        
+
+
 # Define the player O moves
-def player2_move():   
+def player2_move():
     error = 1
     while error == 1:
         try:
             player2_move = int(input(" Please take your move, select an empty space for O : "))
-            if player2_move==board[0]:
+            if player2_move == board[0]:
                 print("\nSorry, that\'s not a valid move. Try again! ")
                 time.sleep(1)
                 player2_move()
@@ -70,17 +70,17 @@ def player2_move():
         os.system('clear')
         print_board(board)
 
-    
+
 # Randomly select which player starts
-def player_select(rp): 
-    rp = random.randint(0,1)
+def player_select(rp):
+    rp = random.randint(0, 1)
     if rp == 0:
         print('Player X will start the game')
         return rp
     else:
         print('Player O will start the game')
         return rp
-        
+
 
 # Check winner rules
 # Check winner X
@@ -97,6 +97,7 @@ def check_win(X):
     else:
         False
 
+
 # Check winner O
 def check_win(O):
     if (board[7] == O and board[8] == O and board[9] == O) or \
@@ -111,20 +112,21 @@ def check_win(O):
     else:
         False
 
+
 # Ask the player for a new game
 def endofgame():
     newgame = input("Would you like to challange somebody again? Y or N? : ")
-    if  newgame == "Y" or newgame == "y":
-            board[0]='-'
-            board[1]='-'
-            board[2]='-'
-            board[3]='-'
-            board[4]='-'
-            board[5]='-'
-            board[6]='-'
-            board[7]='-'
-            board[8]='-'
-            board[9]='-'
+    if newgame == "Y" or newgame == "y":
+            board[0] = '-'
+            board[1] = '-'
+            board[2] = '-'
+            board[3] = '-'
+            board[4] = '-'
+            board[5] = '-'
+            board[6] = '-'
+            board[7] = '-'
+            board[8] = '-'
+            board[9] = '-'
             os.system('clear')
             print_board(board)
             print("Random selecting player...")
@@ -141,22 +143,22 @@ def endofgame():
 # Print the header of the Game
 def print_header():
     header = """
-                88888888888 8888888 .d8888b. 88888888888     d8888  .d8888b. 88888888888 .d88888b.  8888888888            
-                    888       888  d88P  Y88b    888        d88888 d88P  Y88b    888    d88P" "Y88b 888                
-                    888       888  888    888    888       d88P888 888    888    888    888     888 888                
-                    888       888  888           888      d88P 888 888           888    888     888 8888888    
-                    888       888  888           888     d88P  888 888           888    888     888 888        
-                    888       888  888    888    888    d88P   888 888    888    888    888     888 888        
-                    888       888  Y88b  d88P    888   d8888888888 Y88b  d88P    888    Y88b. .d88P 888        
-                    888     8888888 "Y8888P"     888  d88P     888  "Y8888P"     888     "Y88888P"  8888888888 
+                88888888888 8888888 .d8888b. 88888888888     d8888  .d8888b. 88888888888 .d88888b.  8888888888
+                    888       888  d88P  Y88b    888        d88888 d88P  Y88b    888    d88P" "Y88b 888
+                    888       888  888    888    888       d88P888 888    888    888    888     888 888
+                    888       888  888           888      d88P 888 888           888    888     888 8888888
+                    888       888  888           888     d88P  888 888           888    888     888 888
+                    888       888  888    888    888    d88P   888 888    888    888    888     888 888
+                    888       888  Y88b  d88P    888   d8888888888 Y88b  d88P    888    Y88b. .d88P 888
+                    888     8888888 "Y8888P"     888  d88P     888  "Y8888P"     888     "Y88888P"  8888888888
 
 
                                                         +-++-+ +-++-++-++-++-++-++-++-++-++-+ +-+ +-++-++-++-++-+
                                                         |b||y| |s||i||x||p||i||s||t||o||l||s| |&| |G||a||b||e||e|
-                                                        +-++-+ +-++-++-++-++-++-++-++-++-++-+ +-+ +-++-++-++-++-+                 
+                                                        +-++-+ +-++-++-++-++-++-++-++-++-++-+ +-+ +-++-++-++-++-+
 
 
-                            Welcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.           
+                            Welcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.
                             You will make your move by entering a number on the numpad, 1 - 9. The number
                             will correspond to the board position as illustrated:
 
@@ -164,8 +166,8 @@ def print_header():
                                                     ---+---+---
                                                      4 | 5 | 6
                                                     ---+---+---
-                                                     1 | 2 | 3                                                                              
-    
+                                                     1 | 2 | 3
+
                             Prepare yourself! The ultimate battle is about to begin.
 
                             **Press Enter to randomly select the Starting Player!**
@@ -177,7 +179,7 @@ def print_header():
 def main():
     os.system('clear')
     print_board(board)
-    rp = random.randint(0,1)                # Select which player starts
+    rp = random.randint(0, 1)                # Select which player starts
     if rp == 0:
         print('Player X will start the game')
     else:
@@ -185,10 +187,10 @@ def main():
     turn = 0
     while turn < 9:
         if rp == 0:
-            rp+=1
+            rp += 1
             player1_move()
             turn += 1
-            if check_win('X') == True:      # Check for X win
+            if check_win('X') is True:      # Check for X win
                 os.system('clear')
                 print_board(board)
                 print("Congratulations! Player X won!")
@@ -196,14 +198,14 @@ def main():
         else:
             rp -= 1
             player2_move()
-            turn +=1
-            if check_win('O') == True:      # Check for O win
+            turn += 1
+            if check_win('O') is True:      # Check for O win
                 os.system('clear')
                 print_board(board)
                 print("Congratulations! Player O won!")
                 endofgame()
     else:
-        print( "The game is a TIE.")
+        print("The game is a TIE.")
         endofgame()
 
 print_header()
