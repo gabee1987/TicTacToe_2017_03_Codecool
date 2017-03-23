471d72d814106ff9e7b72ee76e747db3caa2b97a
gabee1987 <gabee1987@gmail.com>
2017-03-23 13:59:51 +0100

    xy

--- TicTacToe_master.py
+++ TicTacToe_master.py
@@ -1,4 +1,5 @@
 # Import main modules
+# Import main modules
 import time
 import os
 import random
@@ -164,25 +165,18 @@ def print_header():
                     888       888  888    888    888    d88P   888 888    888    888    888     888 888
                     888       888  Y88b  d88P    888   d8888888888 Y88b  d88P    888    Y88b. .d88P 888
                     888     8888888 "Y8888P"     888  d88P     888  "Y8888P"     888     "Y88888P"  8888888888
-
-
                                                         +-++-+ +-++-++-++-++-++-++-++-++-++-+ +-+ +-++-++-++-++-+
                                                         |b||y| |s||i||x||p||i||s||t||o||l||s| |&| |G||a||b||e||e|
                                                         +-++-+ +-++-++-++-++-++-++-++-++-++-+ +-+ +-++-++-++-++-+
-
-
                             Welcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.
                             You will make your move by entering a number on the numpad, 1 - 9. The number
                             will correspond to the board position as illustrated:
-
                                                      7 | 8 | 9
                                                     ---+---+---
                                                      4 | 5 | 6
                                                     ---+---+---
                                                      1 | 2 | 3
-
                             Prepare yourself! The ultimate battle is about to begin.
-
                             To play against friend write: p
                             To play against computer write: c
     """
